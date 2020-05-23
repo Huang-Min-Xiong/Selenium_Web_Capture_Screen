5b27f63123d1ba43a638f115010233d397fbae3d
黃敏雄 <4a5g0083@stust.edu.tw>
2020-05-24 00:39:51 +0800

    Selenium_Web_Capture_Screen

---
+++ Selenium_Web_Capture_Screen.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+driver=webdriver.Chrome(executable_path=r'C:\Users\user\chromedriver_win32\chromedriver.exe')
+Web_url='https://www.google.com/'
+driver.get(Web_url)
+driver.save_screenshot(r'./Img.png')
